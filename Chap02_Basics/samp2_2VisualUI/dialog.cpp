c8641646cea76524c09548b86438454738f40fdc
jxyanglinus <jxyanglinus@outlook.com>
2024-10-07 16:08:35 +0800

    完成2.2

--- Chap02_Basics/samp2_2VisualUI/dialog.cpp
+++ Chap02_Basics/samp2_2VisualUI/dialog.cpp
@@ -3,13 +3,51 @@
 
 Dialog::Dialog(QWidget *parent)
     : QDialog(parent)
-    , ui(new Ui::Dialog)
-{
+    , ui(new Ui::Dialog) {
     ui->setupUi(this);
+    connect(ui->radioBlack, &QRadioButton::clicked, this, &Dialog::do_setFontColor);
+    connect(ui->radioRed, &QRadioButton::clicked, this, &Dialog::do_setFontColor);
+    connect(ui->radioBlue, &QRadioButton::clicked, this, &Dialog::do_setFontColor);
 }
 
-Dialog::~Dialog()
-{
+Dialog::~Dialog() {
     delete ui;
 }
 
+void Dialog::on_chkBoxUnder_clicked(bool checked) {
+    QFont font = ui->plainTextEdit->font();
+    font.setUnderline(checked);
+    ui->plainTextEdit->setFont(font);
+}
+
+void Dialog::on_chkBoxItalic_clicked(bool checked) {
+    QFont font = ui->plainTextEdit->font();
+    font.setItalic(checked);
+    ui->plainTextEdit->setFont(font);
+}
+
+void Dialog::on_chkBoxBold_clicked(bool checked) {
+    QFont font = ui->plainTextEdit->font();
+    font.setBold(checked);
+    ui->plainTextEdit->setFont(font);
+}
+
+
+void Dialog::on_btnClear_clicked() {
+    ui->plainTextEdit->clear();
+}
+
+void Dialog::do_setFontColor() {
+    //自定义槽函数，设置文字颜色
+    QPalette plet = ui->plainTextEdit->palette();
+    if (ui->radioBlue->isChecked()) {
+        plet.setColor(QPalette::Text, Qt::blue);
+    } else if (ui->radioRed->isChecked()) {
+        plet.setColor(QPalette::Text, Qt::red);
+    } else if (ui->radioBlack->isChecked()) {
+        plet.setColor(QPalette::Text, Qt::black);
+    } else {
+        plet.setColor(QPalette::Text, Qt::black);
+    }
+    ui->plainTextEdit->setPalette(plet);
+}
--- Chap02_Basics/samp2_2VisualUI/dialog.h
+++ Chap02_Basics/samp2_2VisualUI/dialog.h
@@ -4,16 +4,28 @@
 #include <QDialog>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class Dialog; }
+namespace Ui {
+    class Dialog;
+}
 QT_END_NAMESPACE
 
-class Dialog : public QDialog
-{
+class Dialog : public QDialog {
     Q_OBJECT
 
 public:
     Dialog(QWidget *parent = nullptr);
     ~Dialog();
+
+private slots:
+    void on_chkBoxUnder_clicked(bool checked);
+
+    void on_chkBoxItalic_clicked(bool checked);
+
+    void on_chkBoxBold_clicked(bool checked);
+
+    void on_btnClear_clicked();
+
+    void do_setFontColor();
 
 private:
     Ui::Dialog *ui;
